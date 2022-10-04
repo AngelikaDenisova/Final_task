beab0d70c87e3e3d67fff22776ba220ec0ed9e5a
Angelika <anzhelika48@mail.ru>
2022-10-04 09:34:15 +0500

    Решила задачу

---
+++ task/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите массив через пробел: ");
+string[] array = Console.ReadLine()!.Split(' ');
+string[] result = new string[array.Length];
+
+for (int i = 0; i < array.Length; i++)
+{
+    if(array[i].Length <= 3)  
+    {
+      Console.Write(result[i] = array[i] + " ");   
+    }  
+    }
+   
